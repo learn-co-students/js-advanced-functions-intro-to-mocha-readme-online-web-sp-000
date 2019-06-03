e5b57896b3858e7ef47b9804375e883f57ef0758
Joe Ganobsik <jganobsik@gmail.com>
2019-06-03 19:05:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function favoriteIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return "Hey, " + name +"!";
+}
+
+function shouting(word) {
+  return word.toUpperCase();
+}
+function roundDown(num) {
+  return Math.floor(num);
+}
