57768100195a170d79ef14124d60cb6236766fed
Adam Sarli <adam.sarli@gmail.com>
2019-08-21 20:43:48 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function favoriteIceCream(flavor) {
+
+}
+
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(arg) {
+  return arg.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num)
+}
