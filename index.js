e523a4fbb4d58caf95fccf2f9d63dec7e4116320
tan-nate <nktan93@gmail.com>
2019-09-24 10:47:33 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function theTruth() {
+    return true
+}
+function sayHey() {
+    return 'Hey!'
+}
+function sayHeyFriend(friend) {
+    return `Hey, ${friend}!`
+}
+function favoriteIceCream(icecream) {
+    return `I love ${icecream}`
+}
+function shouting(string) {
+    return string.toUpperCase()
+}
+function roundDown(float) {
+    return Math.floor(float)
+}
